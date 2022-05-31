c89b4ae5631d30bd8baa50f0c9add47fbb0b2f3c
Gabriel Savian Zardo <56320217+gabrielsaviank@users.noreply.github.com>
2022-05-31 16:58:01 -0300

    chore: corrected typings

---
+++ frontend/components/UserAvatar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Gravatar from 'react-gravatar';
+
+const UserAvatar = (user: any) => {
+    return(
+        <div>
+
+        </div>
+    );
+};
+
+export default UserAvatar;
---
+++ frontend/components/Users.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useQuery } from "react-apollo";
+import { gql } from "apollo-boost";
+
+import UserAvatar from './UserAvatar';
+// import CreateUser from './createUser';
+
+const GET_USERS = gql`
+ {
+    users {
+        id
+        name
+        email
+        postCount
+    }
+ }
+`;
+
+const Users = ({ selectUser }: { selectUser: () => { this: any, user: string } }) => {
+    const { loading, error, data } = useQuery(GET_USERS)
+    if (loading) return 'Loading...'
+    if (error) return `Error ${error.message}`;
+
+    return (
+        <div className="flex flex-wrap items-center pb-16">
+            {data.users.map((user: { id: string, name:string, email: string, postCount: number }) => {
+                return(
+                    <div
+                        key={user.id}
+                        className="lg:w-1/3 w-full p-4 text-center inline"
+                        onClick={selectUser.bind(this, user)}
+                        >
+                        <UserAvatar user={user}/>
+                    </div>
+                );
+            })}
+            <p>users</p>
+        </div>
+    );
+};
+
+export default Users;
