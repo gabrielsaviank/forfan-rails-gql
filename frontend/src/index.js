5184335aa6ca362d73bef8719ba249283e4b56ab
Gabriel Savian Zardo <56320217+gabrielsaviank@users.noreply.github.com>
2022-05-31 09:43:26 -0300

    chore: apollo setup finished

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient, InMemoryCache } from "apollo-boost";
+import { createHttpLink } from "apollo-link-http";
 
-import './tailwind.css';
 import App from "./App";
+import reportWebVitals from './reportWebVitals';
+import './tailwind.css';
 
+const link = createHttpLink({
+    uri: 'http://localhost:3000/graphql'
+});
+
+const client = new ApolloClient({
+    link: link,
+   cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
