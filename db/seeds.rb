3dea3dc44ee3ccbd55a50007c9a33fc2ca688934
Gabriel Savian Zardo <56320217+gabrielsaviank@users.noreply.github.com>
2022-05-31 08:31:38 -0300

    feat: front-end structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,6 @@
 5.times do
   user = User.create(name: Faker::Name.name, email: Faker::Internet.email)
   5.times do
-    user.posts.create(title: Faker::Lorem.sentence(word_count: 3), body: Faker::Lorem::paragraph(sentence_count: 3))
+    user.posts.create(title: Faker::Lorem.sentence(word_count: 3), body: Faker::Lorem.paragraph(sentence_count: 3))
   end
 end
